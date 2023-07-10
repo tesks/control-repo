46150825eaa07bf25fc0e63ed328ae5b897f545e
tesks <111441006+tesks@users.noreply.github.com>
2023-07-09 18:15:22 -0700

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,13 +1,19 @@
 class profile::r10k {
   class {'r10k':
-    remote => 'https://github.com/joshsamuelson/control-repo',
+    remote => 'https://github.com/tesks/control-repo',
   }
-  class {'r10k::webhook::config':
-    use_mcollective => false,
-    enable_ssl      => false,
-  }
+  class {'r10k::webhook::config': }
   class {'r10k::webhook':
-    user  => 'root',
-    group => 'root',
+    user    => 'root',
+    group   => 'root',
+    chatops => {
+      service    => 'github',
+      repo_owner => 'tesks',
+      repo_name  => 'control-repo',
+      secret     => 'your_webhook_secret',
+    },
+    r10k    => {
+      prefix => 'your_r10k_prefix',
+    },
   }
 }
