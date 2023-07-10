8cf1f207687c67ea8be9f8e42ca51b24707c6f84
tesks <111441006+tesks@users.noreply.github.com>
2023-07-09 21:04:59 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,9 @@
 node puppet.local{
   include role::master
-	file {'/etc/secret_password.txt':
-		ensure => file,
-		content => lookup('secret_password'),
-	}
+  file {'/etc/secret_password.txt':
+	ensure => file,
+	content => lookup('secret_password'),
+  }
 }
 
 
